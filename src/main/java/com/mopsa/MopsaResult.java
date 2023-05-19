1dcd16bbb0d86d9efbd3b111d1499339669b5452
TalebMaham <35927736+TalebMaham@users.noreply.github.com>
2023-05-19 12:41:51 +0200

    Update MopsaResult.java

--- src/main/java/com/mopsa/MopsaResult.java
+++ src/main/java/com/mopsa/MopsaResult.java
@@ -1,84 +1,82 @@
 package com.mopsa;
 
-import org.eclipse.lsp4j.DiagnosticSeverity;
-
 import com.ibm.wala.cast.tree.CAstSourcePositionMap.Position;
 import com.ibm.wala.util.collections.Pair;
-
 import magpiebridge.core.AnalysisResult;
 import magpiebridge.core.Kind;
+import org.eclipse.lsp4j.DiagnosticSeverity;
 
 public class MopsaResult implements AnalysisResult {
-	
-	  private final Kind kind;
-	  private final Position position;
-	  private final String message;
-	  private final Iterable<Pair<Position, String>> related;
-	  private final DiagnosticSeverity severity;
-	  private final Pair<Position, String> repair;
-	  private final String code;
 
-	  public MopsaResult(
-	      Kind kind,
-	      Position pos,
-	      String msg,
-	      Iterable<Pair<Position, String>> relatedInfo,
-	      DiagnosticSeverity severity,
-	      Pair<Position, String> repair,
-	      String code) {
-	    this.kind = kind;
-	    this.position = pos;
-	    this.message = msg;
-	    this.related = relatedInfo;
-	    this.severity = severity;
-	    this.repair = repair;
-	    this.code = code;
-	  }
+  private final Kind kind;
+  private final Position position;
+  private final String message;
+  private final Iterable<Pair<Position, String>> related;
+  private final DiagnosticSeverity severity;
+  private final Pair<Position, String> repair;
+  private final String code;
 
-	  public Kind kind() {
-	    return this.kind;
-	  }
+  public MopsaResult(
+      Kind kind,
+      Position pos,
+      String msg,
+      Iterable<Pair<Position, String>> relatedInfo,
+      DiagnosticSeverity severity,
+      Pair<Position, String> repair,
+      String code) {
+    this.kind = kind;
+    this.position = pos;
+    this.message = msg;
+    this.related = relatedInfo;
+    this.severity = severity;
+    this.repair = repair;
+    this.code = code;
+  }
 
-	  public Position position() {
-	    return position;
-	  }
+  public Kind kind() {
+    return this.kind;
+  }
 
-	  public Iterable<Pair<Position, String>> related() {
-	    return related;
-	  }
+  public Position position() {
+    return position;
+  }
 
-	  public DiagnosticSeverity severity() {
-	    return severity;
-	  }
+  public Iterable<Pair<Position, String>> related() {
+    return related;
+  }
 
-	  public Pair<Position, String> repair() {
-	    return repair;
-	  }
+  public DiagnosticSeverity severity() {
+    return severity;
+  }
 
-	  public String toString(boolean useMarkdown) {
-	    return message;
-	  }
+  public Pair<Position, String> repair() {
+    return repair;
+  }
 
-	  @Override
-	  public String toString() {
-	    return "DataFlowResult [kind="
-	        + kind
-	        + ", position="
-	        + position
-	        + ", code="
-	        + code
-	        + ", message="
-	        + message
-	        + ", related="
-	        + related
-	        + ", severity="
-	        + severity
-	        + ", repair="
-	        + repair
-	        + "]";
-	  }
+  public String toString(boolean useMarkdown) {
+    return message;
+  }
 
-	  public String code() {
-	    return code;
-	  }
+  @Override
+  public String toString() {
+    return "DataFlowResult [kind="
+        + kind
+        + ", position="
+        + position
+        + ", code="
+        + code
+        + ", message="
+        + message
+        + ", related="
+        + related
+        + ", severity="
+        + severity
+        + ", repair="
+        + repair
+        + "]";
+  }
+
+  public String code() {
+    return code;
+  }
 }
