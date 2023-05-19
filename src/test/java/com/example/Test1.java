f7240456a93ad4715a3cf3a9ee247810a74afb48
TalebMaham <35927736+TalebMaham@users.noreply.github.com>
2023-05-19 12:44:52 +0200

    Update Test1.java

--- src/test/java/com/example/Test1.java
+++ src/test/java/com/example/Test1.java
@@ -2,46 +2,55 @@ package com.example;
 
 import static org.junit.Assert.*;
 
+import com.mopsa.MopsaServerAnalysis;
+import java.io.File;
 import org.eclipse.lsp4j.MessageParams;
 import org.eclipse.lsp4j.MessageType;
 import org.junit.Test;
 
-import com.mopsa.MopsaServerAnalysis;
-
 public class Test1 {
 
-	@Test
-	public void test() {
-		
-		
-		MopsaServerAnalysis serverAnalysis = new MopsaServerAnalysis(); 
-		String version = serverAnalysis.getMopsaVersion(); 
-		assertEquals("1.0~pre2 (release)", version);
-	}
-	
-	@Test
-	public void testMessageParamsCreation() {
-	    String result = "1.0~pre2 (release)";
-	    MessageParams message = new MessageParams(MessageType.Info, "Found infer: " + result);
-	    assertEquals(message.getType(), MessageType.Info);
-	    assertEquals(message.getMessage(), "Found infer: 1.0~pre2 (release)");
-	}
-	
-	@Test
-	public void testSource() {
-	    MopsaServerAnalysis server = new MopsaServerAnalysis();
-	    String expectedSource = "mopsa";
-	    String actualSource = server.source();
-	    assertEquals(expectedSource, actualSource);
-	}
+  @Test
+  public void test() {
 
-	@Test
-	public void testJSON() {
-	    MopsaServerAnalysis server = new MopsaServerAnalysis();
-	    String expectedSource = "mopsa";
-	    String actualSource = server.source();
-	    assertEquals(expectedSource, actualSource);
-	}
+    MopsaServerAnalysis serverAnalysis = new MopsaServerAnalysis();
+    String version = serverAnalysis.getMopsaVersion();
+    assertEquals("1.0~pre2 (release)", version);
+  }
 
+  @Test
+  public void testMessageParamsCreation() {
+    String result = "1.0~pre2 (release)";
+    MessageParams message = new MessageParams(MessageType.Info, "Found infer: " + result);
+    assertEquals(message.getType(), MessageType.Info);
+    assertEquals(message.getMessage(), "Found infer: 1.0~pre2 (release)");
+  }
 
+  @Test
+  public void testSource() {
+    MopsaServerAnalysis server = new MopsaServerAnalysis();
+    String expectedSource = "mopsa";
+    String actualSource = server.source();
+    assertEquals(expectedSource, actualSource);
+  }
+
+  @Test
+  public void testJSON() {
+    MopsaServerAnalysis server = new MopsaServerAnalysis();
+    String expectedSource = "mopsa";
+    String actualSource = server.source();
+    assertEquals(expectedSource, actualSource);
+  }
+
+  @Test
+  public void testAnalyseJsonFileExists() {
+    // Chemin relatif du fichier analyse.json
+    String filePath = "src/main/ressources/analyse.json";
+
+    // Crée un objet File à partir du chemin du fichier
+    File file = new File(filePath);
+
+    // Vérifie si le fichier existe
+    assertTrue("Le fichier analyse.json n'existe pas.", file.exists());
+  }
 }
