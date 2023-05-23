fa5e02144192ecfc2a60fa97f129747eb8371ab8
TalebMaham <35927736+TalebMaham@users.noreply.github.com>
2023-05-23 22:07:16 +0200

    Update MopsaIDE.java

--- src/main/java/com/mopsa/MopsaIDE.java
+++ src/main/java/com/mopsa/MopsaIDE.java
@@ -20,88 +20,94 @@ import org.eclipse.lsp4j.jsonrpc.messages.Either;
 public class MopsaIDE {
 
   private static final int DEFAULT_TIMEOUT = 5;
-  private static final String DEFAULT_DOCKER_IMAGE = "jonasmanuel/mopsadocker";
+  private static final String DEFAULT_DOCKER_IMAGE ="mopsadocker";
   private static final String DEFAULT_PORT = "5007";
 
   private static CommandLine cmd = null;
 
   public static void main(String... args) throws IOException, InterruptedException {
-    Options cliOptions =
-        new Options()
-            .addOption(
-                "a",
-                "auto",
-                true,
-                MessageFormat.format(
-                    "enables auto mode and sets the timeout in minutes for starting the automatic tool analysis\n default timeout is {0}",
-                    DEFAULT_TIMEOUT))
-            .addOption(
-                "i",
-                "dockerImage",
-                true,
-                MessageFormat.format(
-                    "dockerImage to use when running mopsa in docker\ndefault is `{0}`",
-                    DEFAULT_DOCKER_IMAGE))
-            .addOption(
-                "s",
-                "socket",
-                false,
-                MessageFormat.format("run in socket mode, standard port is {0}", DEFAULT_PORT))
-            .addOption(
-                "p",
-                "port",
-                true,
-                MessageFormat.format(
-                    "sets the port for socket mode, standard port is {0}", DEFAULT_PORT));
+	    CommandLine cmd = parseCommandLine(args);
+	    if (cmd == null) {
+	        printHelpAndExit();
+	    }
 
-    CommandLineParser parser = new DefaultParser();
-    HelpFormatter formatter = new HelpFormatter();
+	    Supplier<MagpieServer> createServer = createServerSupplier(cmd);
+	    if (cmd.hasOption("socket")) {
+	        int port = getSocketPort(cmd);
+	        MagpieServer.launchOnSocketPort(port, createServer);
+	    } else {
+	        createServer.get().launchOnStdio();
+	    }
+	}
 
-    try {
-      cmd = parser.parse(cliOptions, args);
-    } catch (ParseException e) {
-      formatter.printHelp("MopsaIDE", cliOptions, true);
+	private static CommandLine parseCommandLine(String[] args) {
+	    Options cliOptions = createCLIOptions();
+	    CommandLineParser parser = new DefaultParser();
+	    try {
+	        return parser.parse(cliOptions, args);
+	    } catch (ParseException e) {
+	        return null;
+	    }
+	}
 
-      System.exit(1);
-    }
-    Supplier<MagpieServer> createServer =
-        () -> {
-          boolean auto = false;
-          int timeout = 5;
-          if (cmd.hasOption("auto")) {
-            auto = true;
-            try {
-              timeout = Integer.parseInt(cmd.getOptionValue("auto"));
-            } catch (NumberFormatException e) {
+	private static void printHelpAndExit() {
+	    Options cliOptions = createCLIOptions();
+	    HelpFormatter formatter = new HelpFormatter();
+	    formatter.printHelp("MopsaIDE", cliOptions, true);
+	    System.exit(1);
+	}
 
-              System.exit(1);
-            }
-          }
-          String dockerImage = cmd.getOptionValue("dockerImage", DEFAULT_DOCKER_IMAGE);
+	private static Options createCLIOptions() {
+	    Options cliOptions = new Options();
+	    cliOptions.addOption("a", "auto", true,
+	            MessageFormat.format("enables auto mode and sets the timeout in minutes for starting the automatic tool analysis\n default timeout is {0}", DEFAULT_TIMEOUT));
+	    cliOptions.addOption("i", "dockerImage", true,
+	            MessageFormat.format("dockerImage to use when running infer in docker\ndefault is `{0}`", DEFAULT_DOCKER_IMAGE));
+	    cliOptions.addOption("s", "socket", false,
+	            MessageFormat.format("run in socket mode, standard port is {0}", DEFAULT_PORT));
+	    cliOptions.addOption("p", "port", true,
+	            MessageFormat.format("sets the port for socket mode, standard port is {0}", DEFAULT_PORT));
+	    return cliOptions;
+	}
 
-          ServerConfiguration config = new ServerConfiguration();
-          config.setDoAnalysisBySave(false);
-          if (!auto) {
-            config.setDoAnalysisByOpen(false);
-            config.setShowConfigurationPage(true, true);
-          } else {
-            config.setDoAnalysisByOpen(true);
-            config.setDoAnalysisByIdle(true, timeout * 60 * 1000);
-          }
-          MagpieServer server = new MagpieServer(config);
-          String language = "java";
-          IProjectService javaProjectService = new JavaProjectService();
-          server.addProjectService(language, javaProjectService);
-          ToolAnalysis analysis = new MopsaServerAnalysis();
-          Either<ServerAnalysis, ToolAnalysis> either = Either.forRight(analysis);
-          server.addAnalysis(either, language);
-          return server;
-        };
-    if (cmd.hasOption("socket")) {
-      int port = Integer.parseInt(cmd.getOptionValue("port", DEFAULT_PORT));
-      MagpieServer.launchOnSocketPort(port, createServer);
-    } else {
-      createServer.get().launchOnStdio();
-    }
-  }
+	private static Supplier<MagpieServer> createServerSupplier(CommandLine cmd) {
+	    return () -> {
+	        boolean auto = cmd.hasOption("auto");
+	        int timeout = auto ? parseTimeout(cmd.getOptionValue("auto")) : 5;
+	        String dockerImage = cmd.getOptionValue("dockerImage", DEFAULT_DOCKER_IMAGE);
+
+	        ServerConfiguration config = createServerConfiguration(auto, timeout);
+	        MagpieServer server = new MagpieServer(config);
+	        String language = "java";
+	        IProjectService javaProjectService = new JavaProjectService();
+	        server.addProjectService(language, javaProjectService);
+	        ToolAnalysis analysis = new MopsaServerAnalysis();
+	        Either<ServerAnalysis, ToolAnalysis> either = Either.forRight(analysis);
+	        server.addAnalysis(either, language);
+	        return server;
+	    };
+	}
+
+	private static int getSocketPort(CommandLine cmd) {
+	    return Integer.parseInt(cmd.getOptionValue("port", DEFAULT_PORT));
+	}
+
+	private static ServerConfiguration createServerConfiguration(boolean auto, int timeout) {
+	    ServerConfiguration config = new ServerConfiguration();
+	    config.setDoAnalysisBySave(false);
+	    config.setDoAnalysisByOpen(!auto);
+	    config.setShowConfigurationPage(!auto, true);
+	    config.setDoAnalysisByIdle(auto, timeout * 60 * 1000);
+	    return config;
+	}
+
+	private static int parseTimeout(String value) {
+	    try {
+	        return Integer.parseInt(value);
+	    } catch (NumberFormatException e) {
+	        System.exit(1);
+	        return -1;
+	    }
+	}
+
 }
