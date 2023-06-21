8d4d242f1f30aeeacc876a3fbf2b8e84da60f9d1
TalebMaham <35927736+TalebMaham@users.noreply.github.com>
2023-06-21 10:41:21 +0200

    Update MopsaIDE.java

--- src/main/java/com/mopsa/MopsaIDE.java
+++ src/main/java/com/mopsa/MopsaIDE.java
@@ -20,95 +20,78 @@ import org.eclipse.lsp4j.jsonrpc.messages.Either;
 public class MopsaIDE {
 
   private static final int DEFAULT_TIMEOUT = 5;
-  private static final String DEFAULT_DOCKER_IMAGE ="mopsadocker";
-  private static final String DEFAULT_PORT = "5007";
-
-  private static CommandLine cmd = null;
 
   public static void main(String... args) throws IOException, InterruptedException {
-	    CommandLine cmd = parseCommandLine(args);
-	    if (cmd == null) {
-	        printHelpAndExit();
-	    }
+    CommandLine cmd = parseCommandLine(args);
+    if (cmd == null) {
+      printHelpAndExit();
+    }
 
-	    Supplier<MagpieServer> createServer = createServerSupplier(cmd);
-	    if (cmd.hasOption("socket")) {
-	        int port = getSocketPort(cmd);
-	        MagpieServer.launchOnSocketPort(port, createServer);
-	    } else {
-	        createServer.get().launchOnStdio();
-	    }
-	}
+    Supplier<MagpieServer> createServer = createServerSupplier(cmd);
+    createServer.get().launchOnStdio();
+  }
 
-	private static CommandLine parseCommandLine(String[] args) {
-	    Options cliOptions = createCLIOptions();
-	    CommandLineParser parser = new DefaultParser();
-	    try {
-	        return parser.parse(cliOptions, args);
-	    } catch (ParseException e) {
-	        return null;
-	    }
-	}
+  private static CommandLine parseCommandLine(String[] args) {
+    Options cliOptions = createCLIOptions();
+    CommandLineParser parser = new DefaultParser();
+    try {
+      return parser.parse(cliOptions, args);
+    } catch (ParseException e) {
+      return null;
+    }
+  }
 
-	private static void printHelpAndExit() {
-	    Options cliOptions = createCLIOptions();
-	    HelpFormatter formatter = new HelpFormatter();
-	    formatter.printHelp("MopsaIDE", cliOptions, true);
-	    System.exit(1);
-	}
+  private static void printHelpAndExit() {
+    Options cliOptions = createCLIOptions();
+    HelpFormatter formatter = new HelpFormatter();
+    formatter.printHelp("MopsaIDE", cliOptions, true);
+    System.exit(1);
+  }
 
-	private static Options createCLIOptions() {
+  private static Options createCLIOptions() {
     Options cliOptions = new Options();
-    cliOptions.addOption("a", "auto", true,
-            MessageFormat.format("active le mode automatique et définit le délai d'attente en minutes pour démarrer l'analyse automatique des outils\n le délai d'attente par défaut est {0}", DEFAULT_TIMEOUT));
-    cliOptions.addOption("i", "dockerImage", true,
-            MessageFormat.format("image Docker à utiliser lors de l'exécution de mopsa en Docker\n par défaut : `{0}`", DEFAULT_DOCKER_IMAGE));
-    cliOptions.addOption("s", "socket", false,
-            MessageFormat.format("exécute en mode socket, le port standard est {0}", DEFAULT_PORT));
-    cliOptions.addOption("p", "port", true,
-            MessageFormat.format("définit le port pour le mode socket, le port standard est {0}", DEFAULT_PORT));
+    cliOptions.addOption(
+        "a",
+        "auto",
+        true,
+        MessageFormat.format(
+            "active le mode automatique et définit le délai d'attente en minutes pour démarrer l'analyse automatique des outils\n le délai d'attente par défaut est {0}",
+            DEFAULT_TIMEOUT));
     return cliOptions;
-}
+  }
 
+  private static Supplier<MagpieServer> createServerSupplier(CommandLine cmd) {
+    return () -> {
+      boolean auto = cmd.hasOption("auto");
+      int timeout = auto ? parseTimeout(cmd.getOptionValue("auto")) : 5;
 
-	private static Supplier<MagpieServer> createServerSupplier(CommandLine cmd) {
-	    return () -> {
-	        boolean auto = cmd.hasOption("auto");
-	        int timeout = auto ? parseTimeout(cmd.getOptionValue("auto")) : 5;
-	        String dockerImage = cmd.getOptionValue("dockerImage", DEFAULT_DOCKER_IMAGE);
+      ServerConfiguration config = createServerConfiguration(auto, timeout);
+      MagpieServer server = new MagpieServer(config);
+      String language = "java";
+      IProjectService javaProjectService = new JavaProjectService();
+      server.addProjectService(language, javaProjectService);
+      ToolAnalysis analysis = new MopsaServerAnalysis();
+      Either<ServerAnalysis, ToolAnalysis> either = Either.forRight(analysis);
+      server.addAnalysis(either, language);
+      return server;
+    };
+  }
 
-	        ServerConfiguration config = createServerConfiguration(auto, timeout);
-	        MagpieServer server = new MagpieServer(config);
-	        String language = "java";
-	        IProjectService javaProjectService = new JavaProjectService();
-	        server.addProjectService(language, javaProjectService);
-	        ToolAnalysis analysis = new MopsaServerAnalysis();
-	        Either<ServerAnalysis, ToolAnalysis> either = Either.forRight(analysis);
-	        server.addAnalysis(either, language);
-	        return server;
-	    };
-	}
+  private static ServerConfiguration createServerConfiguration(boolean auto, int timeout) {
+    ServerConfiguration config = new ServerConfiguration();
+    config.setDoAnalysisBySave(false);
+    config.setDoAnalysisByOpen(!auto);
+    config.setShowConfigurationPage(!auto, true);
+    config.setDoAnalysisByIdle(auto, timeout * 60 * 1000);
+    return config;
+  }
 
-	private static int getSocketPort(CommandLine cmd) {
-	    return Integer.parseInt(cmd.getOptionValue("port", DEFAULT_PORT));
-	}
-
-	private static ServerConfiguration createServerConfiguration(boolean auto, int timeout) {
-	    ServerConfiguration config = new ServerConfiguration();
-	    config.setDoAnalysisBySave(false);
-	    config.setDoAnalysisByOpen(!auto);
-	    config.setShowConfigurationPage(!auto, true);
-	    config.setDoAnalysisByIdle(auto, timeout * 60 * 1000);
-	    return config;
-	}
-
-	private static int parseTimeout(String value) {
-	    try {
-	        return Integer.parseInt(value);
-	    } catch (NumberFormatException e) {
-	        System.exit(1);
-	        return -1;
-	    }
-	}
-
+  private static int parseTimeout(String value) {
+    try {
+      return Integer.parseInt(value);
+    } catch (NumberFormatException e) {
+      System.exit(1);
+      return -1;
+    }
+  }
 }
