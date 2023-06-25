78bf4b406838bcb228a8dc15ea25763e6063c6d9
TalebMaham <35927736+TalebMaham@users.noreply.github.com>
2023-06-26 00:48:05 +0200

    Update CProjectService.java

--- src/main/java/com/mopsa/CProjectService.java
+++ src/main/java/com/mopsa/CProjectService.java
@@ -1,94 +1,32 @@
-
 package com.mopsa;
 
-// import magpiebridge.util.FileUtils;
+import java.nio.file.Path;
+import java.util.Optional;
+import magpiebridge.core.IProjectService;
 
-public class CProjectService /*implements IProjectService*/ {
+public class CProjectService implements IProjectService {
 
-  // private Optional<Path> rootPath;
-  // private Set<Path> sourcePath;
-  // private Set<Path> includePath;
-  // private Set<Path> libraryPath;
-  // private Set<String> externalDependencies;
+  private Optional<Path> rootPath;
 
-  // public CProjectService() {
-  //   this.sourcePath = Collections.emptySet();
-  //   this.includePath = Collections.emptySet();
-  //   this.libraryPath = Collections.emptySet();
-  //   this.externalDependencies = Collections.emptySet();
-  //   this.rootPath = Optional.empty();
-  // }
+  public CProjectService() {
+    System.out.println("Cprojet : Constructeur ! ");
+    this.rootPath = Optional.empty();
+  }
 
-  // public CProjectService(
-  //     Set<Path> sourcePath,
-  //     Set<Path> includePath,
-  //     Set<Path> libraryPath,
-  //     Set<String> externalDependencies) {
-  //   this();
-  //   this.sourcePath = sourcePath;
-  //   this.includePath = includePath;
-  //   this.libraryPath = libraryPath;
-  //   this.externalDependencies = externalDependencies;
-  // }
+  public Optional<Path> getRootPath() {
+    System.out.println("Cprojet : Getter! ");
+    return rootPath;
+  }
 
-  // public Set<Path> getSourcePath() {
-  //   return sourcePath;
-  // }
+  @Override
+  public void setRootPath(Path rootPath) {
+    System.out.println("Cprojet : Setter! ");
+    this.rootPath = Optional.ofNullable(rootPath);
+  }
 
-  // public Set<Path> getIncludePath() {
-  //   return includePath;
-  // }
-
-  // public Set<Path> getLibraryPath() {
-  //   return libraryPath;
-  // }
-
-  // public Set<String> getExternalDependencies() {
-  //   return externalDependencies;
-  // }
-
-  // public Optional<Path> getRootPath() {
-  //   return rootPath;
-  // }
-
-  // @Override
-  // public void setRootPath(Path rootPath) {
-  //   this.rootPath = Optional.ofNullable(rootPath);
-  // }
-
-  // public Map<String, Path> getSources() {
-  //   Map<String, Path> sources = new HashMap<>();
-  //   /* for (Path source : sourcePath) {
-  //     if (rootPath.isPresent()) {
-  //       Path absoluteSource = rootPath.get().resolve(source);
-  //       FileUtils.collectFiles(absoluteSource, ".c", true)
-  //           .forEach(file -> sources.put(file.getFileName().toString(), file));
-  //     }
-  //   }*/
-  //   return sources;
-  // }
-
-  // public Map<String, Path> getHeaders() {
-  //   Map<String, Path> headers = new HashMap<>();
-  //   for (Path include : includePath) {
-  //     // if (rootPath.isPresent()) {
-  //     //   Path absoluteInclude = rootPath.get().resolve(include);
-  //     //   FileUtil.collectFiles(absoluteInclude, ".h", true)
-  //     //       .forEach(file -> headers.put(file.getFileName().toString(), file));
-  //     // }
-  //   }
-  //   return headers;
-  // }
-
-  // public Process executeCommand(String command) throws IOException {
-  //   ProcessBuilder processBuilder = new ProcessBuilder(command.split(" "));
-  //   processBuilder.directory(rootPath.orElse(null));
-  //   return processBuilder.start();
-  // }
-
-  // @Override
-  // public String getProjectType() {
-  //   // TODO Auto-generated method stub
-  //   return "c";
-  // }
+  @Override
+  public String getProjectType() {
+    System.out.println("Cprojet : ProjectType ");
+    return "c";
+  }
 }
